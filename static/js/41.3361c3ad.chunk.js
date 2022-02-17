0418e90b4ff40ab7ea9ac32bea6fdf16f2548008
nutz <MY_NAME@example.com>
2022-02-17 04:42:28 -0600

    Updates

---
+++ static/js/41.3361c3ad.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[41],{1790:function(n,p){}}]);
+//# sourceMappingURL=41.3361c3ad.chunk.js.map
--- static/js/41.f8f532ff.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[41],{1784:function(n,p){}}]);
-//# sourceMappingURL=41.f8f532ff.chunk.js.map
