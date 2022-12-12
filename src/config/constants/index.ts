9997a385a4cdf4f2839ad392618216696a8b65a5
Kiba Inu <116835903+TheKibaInu@users.noreply.github.com>
2022-12-11 22:03:34 -0500

    Update index.ts

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token } from '@pancakeswap/sdk'
 import { mainnetTokens, testnetTokens } from './tokens'
 
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+export const ROUTER_ADDRESS = '0xacb85595c219e7FC92623A0934eD72D7968d9Bf4'
 
 // a list of tokens by chain
 type ChainTokenList = {
